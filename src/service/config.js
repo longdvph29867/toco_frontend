b9748620d3d573dc3d2028560345132bdeecc0b9
longdv ph29867 <emchuaroinhe@gmail.com>
2023-10-28 16:34:13 +0700

    update

--- src/service/config.js
+++ src/service/config.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { localUserService } from "./localService";
 
 export const BASE_URL = 'https://toco-backend.vercel.app';
 const configHeadres = {
-    "Authorization": "Bearer" + "123"
+    "Authorization": "Bearer " + localUserService.get().accessToken,
 }
 export const https = axios.create({
     baseURL: BASE_URL,
